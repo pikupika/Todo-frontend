6d7d2deb6ee5845e0511d6abf74a9631c459db17
pikupika <peeyuvi2002@gmail.com>
2025-07-26 23:30:48 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Home from './pages/Home';
+
+export default function App() {
+  const token = localStorage.getItem('token');
+
+  return (
+    <Routes>
+      <Route path='/' element={token ? <Home /> : <Navigate to='/login'/>} />
+      <Route path='/login' element={<Login/>} />
+      <Route path='/register' element={<Register />} />
+    </Routes>
+  )
+}
