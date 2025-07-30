ec798ea51355dcb86ac6b8a85ae073fa3454ef40
pikupika <peeyuvi2002@gmail.com>
2025-07-30 13:02:08 +0530

    resolved blankpage

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 const Register = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const navigate = useNavigate();
+  const navigate = navigate();
 
   const handleRegister = async () => {
     if (!email) {
